49b03dcbc1858bce71ca048f4b25f7b5f5f04538
Sergei Barbarash <sgt.sgt@gmail.com>
2009-06-04 18:53:05 +0200

    changed migration name

--- generators/acts_as_featured_migration/acts_as_featured_migration_generator.rb
+++ generators/acts_as_featured_migration/acts_as_featured_migration_generator.rb
@@ -1,7 +1,7 @@
 class ActsAsFeaturedMigrationGenerator < Rails::Generator::Base
   def manifest
     record do |m|
-      m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => "acts_as_featured_migration"
+      m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => "create_features"
     end
   end
 end
